a13a010c2d3d6ceea3258c3b950eb7d7a676861a
yt-liceDev <muh.sholeh.dev@gmail.com>
2023-11-21 10:34:20 +0900

    add gitignore

---
+++ prisma/migrations/20231121012305_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Gathering" (
+    "id" STRING NOT NULL,
+    "title" STRING NOT NULL,
+    "address" STRING NOT NULL,
+    "description" STRING NOT NULL,
+
+    CONSTRAINT "Gathering_pkey" PRIMARY KEY ("id")
+);
